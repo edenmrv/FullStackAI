278e4f550c6b8e3224e24be1811daae06979dc55
edenmrv <edenmrv@gmail.com>
2026-07-11 16:26:42 +0300

    EX2

--- MobX & React‏/src/components/Reservation.jsx
+++ MobX & React‏/src/components/Reservation.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import { observer, inject } from 'mobx-react'
 
 class Reservation extends Component {
+  seat = () => {
+    this.props.RestaurantStore.seatRes(this.props.res.id)
+  }
+
+  complete = () => {
+    this.props.RestaurantStore.completeRes(this.props.res.id)
+  }
+
   render() {
+    const { res } = this.props
     return (
-      <div></div>
+      <div>
+        <span>{res.name}</span>
+        <span> - {res.numPeople} people</span>
+        <button onClick={this.seat}>Seat Reservation</button>
+        <button onClick={this.complete}>Complete Reservation</button>
+      </div>
     )
   }
 }
 
-export default Reservation
+export default inject('RestaurantStore')(observer(Reservation))
--- MobX & React‏/src/components/Restaurant.jsx
+++ MobX & React‏/src/components/Restaurant.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
 import ResInput from './ResInput'
+import Reservation from './Reservation'
 
 class Restaurant extends Component {
+  addRes = () => {
+    const { GeneralStore, RestaurantStore } = this.props
+    RestaurantStore.addRes(GeneralStore.name, GeneralStore.numPeople)
+  }
+
   render() {
+    const { RestaurantStore } = this.props
     return (
       <div>
-        <span>You have {this.props.RestaurantStore.openTables} open tables</span>
+        <div>You have {RestaurantStore.openTables} open tables</div>
+        <div>{RestaurantStore.restPopulation} people in the restaurant</div>
+        <div id="completedTables">{RestaurantStore.completedTables} completed tables</div>
+
         <ResInput />
-        <button id="addRes">Add Reservation</button>
-        <div className="reservations"></div>
+        <button id="addRes" onClick={this.addRes}>Add Reservation</button>
+
+        <div className="reservations">
+          {RestaurantStore.reservations.map(res => (
+            <Reservation key={res.id} res={res} />
+          ))}
+        </div>
       </div>
     )
   }
